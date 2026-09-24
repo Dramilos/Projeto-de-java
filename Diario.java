1e28e5abd878c75a8007f7e7a11be44852df1b7c
Leonardo Rogério <dramsanima@gmail.com>
2026-09-24 15:08:25 -0300

    Código adequado pra programação em objetos
    agora a maior parte do código fica na classe diário

--- Diario.java
+++ Diario.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Diario
@@ -7,7 +6,8 @@ public class Diario
     // atributos da classe
     String nomeClasse;
     int quantNotas;
-    ArrayList<Aluno> lista;
+    static ArrayList<Aluno> lista;
+    static Diario turma;
 
     // construtor pra classe diário
     public Diario (String nomeClasse, int quantNotas)
@@ -17,25 +17,99 @@ public class Diario
         lista = new ArrayList<>();
     }
 
-    // método pra criar um aluno novo
-    public void adicionaAluno (Aluno kid)
+    // criador de diários
+    public static void criarDiario()
     {
-        lista.add(kid);
+        int quantNotas;
+        String nomeClasse;
+
+        Principal.fflush();
+
+        System.out.println("\nQual o nome da turma? ");
+        nomeClasse = Principal.teclado.nextLine();
+
+        System.out.println("\nQuantas notas tem cada aluno? ");
+        quantNotas = Principal.teclado.nextInt();
+
+        // criar objeto diário de classe
+        turma = new Diario(nomeClasse, quantNotas);
+    }
+
+    // método para adicionar alunos a lista
+    public static void adicionarAlunos()
+    {
+        Principal.fflush();
+
+        int quantAlunos;
+
+        System.out.println("\nQuantos alunos deseja adicionar? ");
+        quantAlunos = Principal.teclado.nextInt();
+
+        for (int i = 0; i < quantAlunos; i++)
+        {
+            String nome;
+            int RA, nota;
+
+            Principal.teclado.nextLine();      // limpar o buffer
+
+            System.out.println("\n-=-=- ALUNO " + (i + 1) + " -=-=-=-");
+
+            System.out.println("\n -NOME: ");
+            nome = Principal.teclado.nextLine();
+
+            System.out.println("\n -RA: ");
+            RA = Principal.teclado.nextInt();
+
+            Aluno kid = new Aluno(nome, turma.quantNotas, RA);
+
+            for (int j = 0; j < turma.quantNotas ; j++)
+            {
+                System.out.println("\n -DIGITE A NOTA " + (j + 1));
+                do
+                {
+                    nota = Principal.teclado.nextInt();
+
+                    if(nota < 0 || nota > 100)
+                    {
+                        System.out.println("\n Valor inválido!");
+                    }
+
+                } while(nota < 0 || nota > 100);
+
+                kid.setNotas(j, nota);
+            }
+            lista.add(kid);
+        }
+
+    }
+
+
+    // método que imprime um relatório
+    public static void relatorio()
+    {
+        Principal.fflush();
+
+        System.out.println("RELATÓRIO");
+        
+        for(int i = 0; i < lista.size(); i++)
+        {
+            System.out.println("=========\nALUNO " + (i + 1));
+            System.out.println("\n -Nome: " + lista.get(i).getNome());
+            for(int j = 0; j < turma.quantNotas; j++)
+            {
+                System.out.println("\t - nota " + (j + 1) + ": " + lista.get(i).getNotas(j));
+            }
+            System.out.println("\n -Média: " + lista.get(i).getMedia());
+        }
+
+        System.out.println("Digite qualquer tecla para sair");
+        Principal.teclado.nextLine();
     }
 
     // retorna as médias dos alunos
     public void medias()
     {
-        for(int i = 0; i < lista.size(); i++)
-        {
-            System.out.println("=========\nALUNO " + (i + 1));
-            System.out.println("\n -Nome: " + lista.get(i).getNome());
-            for(int j = 0; j < quantNotas; j++)
-            {
-                System.out.println("\t - nota " + (j + 1) + ": " + lista.get(j).getNotas(j));
-            }
-            System.out.println("\n -Média: " + lista.get(i).getMedia());
-        }
+        
     }
 
     // métodos para definir as notas de cada aluno
